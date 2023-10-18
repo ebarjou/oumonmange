6eb2cacdf4acac3221bed09c5ea5805050ffba94
ebarjou <e.barjousuire@gmail.com>
2023-10-18 22:47:09 +0200

    Create list.js

---
+++ list.js
@@ -0,0 +1,167 @@
+data = [
+    {
+        "label": "Makito",
+        "closed_day": "",
+        "tag": "froid,sushi"
+    },
+    {
+        "label": "Eat Salad",
+        "closed_day": "",
+        "tag": "froid,salade"
+    },
+    {
+        "label": "MacDo",
+        "closed_day": "",
+        "tag": "chaud,gras,burger"
+    },
+    {
+        "label": "Santosha",
+        "closed_day": "",
+        "tag": "chaud,asiatique"
+    },
+    {
+        "label": "Bistrot Régent",
+        "closed_day": "",
+        "tag": "chaud,francais"
+    },
+    {
+        "label": "Alcala",
+        "closed_day": "",
+        "tag": "chaud,pizza"
+    },
+    {
+        "label": "K-Thai",
+        "closed_day": "",
+        "tag": "chaud,froid,asiatique"
+    },
+    {
+        "label": "Street burger",
+        "closed_day": "",
+        "tag": "chaud,gras,burger"
+    },
+    {
+        "label": "Pok'n Bowl",
+        "closed_day": "",
+        "tag": "chaud,froid,poke"
+    },
+    {
+        "label": "Sur le pouce",
+        "closed_day": "",
+        "tag": "chaud,froid,emporter,sandwich"
+    },
+    {
+        "label": "Les halles",
+        "closed_day": "",
+        "tag": "chaud,froid"
+    },
+    {
+        "label": "Chez Emile",
+        "closed_day": "",
+        "tag": ""
+    },
+    {
+        "label": "Boulangerie Basque",
+        "closed_day": "",
+        "tag": "froid,emporter,sandwich"
+    },
+    {
+        "label": "Kebab forum",
+        "closed_day": "",
+        "tag": "chaud,gras,kebab"
+    },
+    {
+        "label": "Angelus",
+        "closed_day": "lundi,mardi,mercredi,jeudi",
+        "tag": "chaud,foodtruck"
+    },
+    {
+        "label": "Yu Teppanyaki",
+        "closed_day": "",
+        "tag": "chaud,asiatique"
+    },
+    {
+        "label": "Pitaya",
+        "closed_day": "",
+        "tag": "chaud,asiatique"
+    },
+    {
+        "label": "Aladin Kebab",
+        "closed_day": "",
+        "tag": "chaud,gras,kebab"
+    },
+    {
+        "label": "BBQ Wok",
+        "closed_day": "",
+        "tag": "chaud,asiatique"
+    },
+    {
+        "label": "CFC",
+        "closed_day": "",
+        "tag": "chaud,gras,burger"
+    },
+    {
+        "label": "Yamato",
+        "closed_day": "lundi",
+        "tag": "chaud,asiatique,sushi"
+    },
+    {
+        "label": "O'Tacos",
+        "closed_day": "",
+        "tag": "chaud,gras"
+    },
+    {
+        "label": "Carpe Diem",
+        "closed_day": "",
+        "tag": "chaud,francais"
+    },
+    {
+        "label": "Burger King",
+        "closed_day": "",
+        "tag": "chaud,gras,burger"
+    },
+    {
+        "label": "48",
+        "closed_day": "",
+        "tag": "froid,sandwich,emporter"
+    },
+    {
+        "label": "Kobo Sushi",
+        "closed_day": "",
+        "tag": "froid,asiatique,sushi"
+    },
+    {
+        "label": "Fac Kebab",
+        "closed_day": "",
+        "tag": "chaud"
+    },
+    {
+        "label": "Krousty",
+        "closed_day": "",
+        "tag": "chaud,asiatique,gras"
+    },
+    {
+        "label": "Twist'n creps",
+        "closed_day": "lundi",
+        "tag": "chaud,francais"
+    },
+    {
+        "label": "Poke Moon",
+        "closed_day": "",
+        "tag": "froid,poke"
+    },
+    {
+        "label": "Pizzeria du marché",
+        "closed_day": "",
+        "tag": "chaud,pizza,italien"
+    },
+    {
+        "label": "Rooster",
+        "closed_day": "",
+        "tag": "chaud,burger,gras"
+    },
+    {
+        "label": "Ragazzi Di Peppone",
+        "closed_day": "",
+        "tag": "chaud,italien"
+    },
+  ];
